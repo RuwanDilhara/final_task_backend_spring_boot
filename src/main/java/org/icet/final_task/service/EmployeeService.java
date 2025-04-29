9d23793a7dcf769ad6eff7e7206ebc1af21e44b4
RuwanDilhara <roshansameera916@gmail.com>
2025-04-29 10:31:26 +0530

    created the  EmployeeService class

--- src/main/java/org/icet/final_task/service/EmployeeService.java
+++ src/main/java/org/icet/final_task/service/EmployeeService.java
@@ -1,4 +1,16 @@
 package org.icet.final_task.service;
 
+import org.icet.final_task.dto.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+    List<Employee> getAll();
+    Employee getById(Integer id);
+    Employee getByEmail(String email);
+    Employee create(Employee employee);
+    Employee update(Employee employee);
+    boolean deleteById(Integer id);
+    List<Employee> getByDepartment(String department);
+    List<Employee> getByName(String name);
 }
