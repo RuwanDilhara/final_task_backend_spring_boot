bee0410e2b2c7e0fe4aa69f2ee620a5dfbfd7cac
RuwanDilhara <roshansameera916@gmail.com>
2025-04-29 10:43:53 +0530

    updated the EmployeeController class

--- src/main/java/org/icet/final_task/controller/EmployeeController.java
+++ src/main/java/org/icet/final_task/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package org.icet.final_task.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.icet.final_task.dto.Employee;
 import org.icet.final_task.service.EmployeeService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -12,5 +15,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/employees")
 public class EmployeeController {
     final EmployeeService service;
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Employee>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getAll());
+    }
+
+    @GetMapping("/getById")
+    public ResponseEntity<Employee> getById(@RequestParam ( name = "id" ,required = true ) Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getById(id));
+    }
+
+    @GetMapping("/getByEmail")
+    public ResponseEntity<Employee> getByEmail(@RequestParam ( name = "email" ,required = true )String email){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getByEmail(email));
+    }
+
+    @GetMapping("getByDepartment")
+    public ResponseEntity<List<Employee>> getByDepartment(@RequestParam ( name = "department" ,required = true ) String department){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getByDepartment(department));
+    }
+
+    @GetMapping("/getByName")
+    public ResponseEntity<List<Employee>> getByName(@RequestParam ( name = "name" ,required = true )String name){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getByName(name));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Employee> save(@RequestBody Employee employee){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(employee));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> update(@RequestBody Employee employee){
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(employee));
+    }
+
+    @DeleteMapping("/deleteById")
+    public ResponseEntity<Boolean> deleteById(@RequestParam ( name = "id" ,required = true )Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(service.deleteById(id));
+    }
 
 }
