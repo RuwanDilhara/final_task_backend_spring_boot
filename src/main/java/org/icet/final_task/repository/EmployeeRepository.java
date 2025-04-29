2705b00fee37fe9de6b5c7cd9187763be4edec10
RuwanDilhara <roshansameera916@gmail.com>
2025-04-29 10:55:15 +0530

    updated the EmployeeRepository interface

--- src/main/java/org/icet/final_task/repository/EmployeeRepository.java
+++ src/main/java/org/icet/final_task/repository/EmployeeRepository.java
@@ -1,7 +1,17 @@
 package org.icet.final_task.repository;
 
+import org.icet.final_task.dto.Employee;
 import org.icet.final_task.entity.EmployeeEntity;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Arrays;
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity ,Integer> {
+    EmployeeEntity findByEmail(String email);
+
+    List<EmployeeEntity> findByDepartment(String department);
+
+    List<EmployeeEntity>findByName(String name);
 }
