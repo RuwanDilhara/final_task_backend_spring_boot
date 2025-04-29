a29a3dfac700dfb7216757268480c612dca6ba58
RuwanDilhara <roshansameera916@gmail.com>
2025-04-29 10:33:05 +0530

    created the EmployeeService class

--- src/main/java/org/icet/final_task/service/impl/EmployeeImpl.java
+++ src/main/java/org/icet/final_task/service/impl/EmployeeImpl.java
@@ -1,8 +1,50 @@
 package org.icet.final_task.service.impl;
 
+import org.icet.final_task.dto.Employee;
 import org.icet.final_task.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeImpl implements EmployeeService {
+    @Override
+    public List<Employee> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public Employee getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Employee getByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public Employee create(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Employee update(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Integer id) {
+        return false;
+    }
+
+    @Override
+    public List<Employee> getByDepartment(String department) {
+        return List.of();
+    }
+
+    @Override
+    public List<Employee> getByName(String name) {
+        return List.of();
+    }
 }
